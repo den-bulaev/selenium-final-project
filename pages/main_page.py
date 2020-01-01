89d391cd836c4346c86e72be064e8ceb754dc1ef
python-dude <all.nickname.is.busy@gmail.com>
2020-01-01 17:31:11 +0200

    should_be_login_link method added

---
+++ pages/main_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class MainPage(BasePage):
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(By.ID, "login_link")
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "login link is not presented"
