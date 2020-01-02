2111714d592f6aaf5232f1cf731b20bb0ade58e9
python-dude <all.nickname.is.busy@gmail.com>
2020-01-02 14:55:51 +0200

    new page(login page) available for tet

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,16 @@
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()         # открываем страницу
+    page.should_be_login_link()
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+# запускаем тест командой: pytest -v --tb=line --language=en test_main_page.py
+# предварительно не забыв перейти в нужную директорию
