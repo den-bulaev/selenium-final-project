90e5656eaf954690c586bb1309164fd18cabd532
python-dude <all.nickname.is.busy@gmail.com>
2020-01-02 15:03:17 +0200

    update selectors from locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,13 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        link = self.browser.find_element(By.ID, "login_link")
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "login link is not presented"
