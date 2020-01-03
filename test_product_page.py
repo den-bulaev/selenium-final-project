5c80f323aff34bb9917e1a552a36a54a8bd62751
python-dude <all.nickname.is.busy@gmail.com>
2020-01-03 23:38:16 +0200

    test update to catch a bug

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,12 @@
 from pages.product_page import ProductPage
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
-
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize('promo', ["0", "1", "2", "3", "4", "5", "6",
+                                   pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com"
+                                "/catalogue/coders-at-work_207/?promo=offer{}".format(promo))
     page.open()
     page.go_to_product_page()
     page.solve_quiz_and_get_code()
