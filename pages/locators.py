31e008f5a18042e1e1f3a4f1110cbaaf6b517133
python-dude <all.nickname.is.busy@gmail.com>
2020-01-03 20:44:01 +0200

    locators for product page added

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
+class ProductPageLocators:
+    BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    ADDING_MESSAGE = (By.CSS_SELECTOR, "#messages>:nth-child(1)>.alertinner")
+    PRODUCT_NAME_MESSAGE = (By.CSS_SELECTOR, "#messages>:nth-child(1)>.alertinner>strong")
+    TOTAL_PRICE_MESSAGE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
