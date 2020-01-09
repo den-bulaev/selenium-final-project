de26cb6fa125208f866401bee564b1f1e4033c84
python-dude <all.nickname.is.busy@gmail.com>
2020-01-09 19:26:49 +0200

    new user registration method added

--- pages/login_page.py
+++ pages/login_page.py
@@ -19,3 +19,22 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "register form is not presented"
+
+    def register_new_user(self, email, password):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL), "email field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD), "password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_REPEAT_PASSWORD),\
+            "repeat password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON), "registration button is not presented"
+
+        mail = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        mail.send_keys(email)
+
+        password_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD)
+        password_field.send_keys(password)
+
+        repeat_password = self.browser.find_element(*LoginPageLocators.REGISTER_REPEAT_PASSWORD)
+        repeat_password.send_keys(password)
+
+        register_btn = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_btn.click()
