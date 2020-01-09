95a1b327c66f94a8df0a8600cf6a609a8974454e
python-dude <all.nickname.is.busy@gmail.com>
2020-01-09 19:12:28 +0200

    put things in order

--- test_main_page.py
+++ test_main_page.py
@@ -6,15 +6,12 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
-    page.open()         # открываем страницу
+    page = MainPage(browser, link)
+    page.open()
     page.should_be_login_link()
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-
+    page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-# запускаем тест командой: pytest -v --tb=line --language=en test_main_page.py
-# предварительно не забыв перейти в нужную директорию
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
