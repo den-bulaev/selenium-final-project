bcc618cd6bd612374425abab56ca33e36b504514
python-dude <all.nickname.is.busy@gmail.com>
2020-01-09 19:21:14 +0200

    added class with tests for authorized users

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,13 @@
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 import pytest
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo', ["0", "1", "2", "3", "4", "5", "6",
                                    pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo):
@@ -13,13 +16,11 @@ def test_guest_can_add_product_to_basket(browser, promo):
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-
     basket_page = ProductPage(browser, browser.current_url)
     basket_page.expected_results()
 
 
 @pytest.mark.xfail
-@pytest.mark.smoke
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -27,7 +28,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
-@pytest.mark.smoke
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link, timeout=0)
     page.open()
@@ -35,7 +35,6 @@ def test_guest_cant_see_success_message(browser):
 
 
 @pytest.mark.xfail
-@pytest.mark.smoke
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -43,21 +42,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.element_should_be_fade()
 
 
-@pytest.mark.from_product_to_login
 def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
-@pytest.mark.from_product_to_login
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
-@pytest.mark.basket
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -65,3 +63,27 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page2 = BasketPage(browser, browser.current_url)
     page2.basket_should_be_empty()
     page2.must_be_text_that_the_basket_is_empty()
+
+
+class TestUserAddToBasketFromProductPage:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        login_page = LoginPage(browser, "http://selenium1py.pythonanywhere.com/accounts/login/")
+        login_page.open()
+        login_page.register_new_user(email, "QWERTY98765")
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link, timeout=0)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        basket_page = ProductPage(browser, browser.current_url)
+        basket_page.expected_results()
